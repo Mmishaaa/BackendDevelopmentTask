ebf8866851ecf553b379c16d0be866cae7fc73b8
Misha <mikhail.kochyn@b2tech.com>
2025-09-04 22:19:21 +0200

    Update Program.cs

--- BackendDevelopmentTask/BackendDevelopmentTask/Program.cs
+++ BackendDevelopmentTask/BackendDevelopmentTask/Program.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 using BackendDevelopmentTask.API.Extensions;
+using BackendDevelopmentTask.DAL;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,18 @@ builder.Services.AddControllers().AddJsonOptions(x =>
 builder.Services.AddAPIDependencies(builder.Configuration, builder.Environment);
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    var logger = app.Services.GetRequiredService<ILogger<Program>>();
+    logger.LogInformation("Ensuring database is created...");
+    
+    await using var scope = app.Services.CreateAsyncScope();
+    await using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+    
+    logger.LogInformation("Database initialization completed");
+}
 
 app.UseExceptionMiddleware();
 
